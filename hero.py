22bddfde07eb442bf17e5b83109d07f43f16c6ac
Lartg <roc.gerrety@gmail.com>
2021-10-29 13:23:47 -0800

    added stuff to hero class

--- hero.py
+++ hero.py
@@ -23,17 +23,41 @@ class Hero:
   def add_ability(self, ability):
     self.abilities.append(ability)
 
+  def attack(self):
+    damage_output = 0
+    for ability in self.abilities:
+      damage_output += ability.attack()
+    return damage_output
+
+  def add_armor(self, armor):
+    self.armor.append(armor)
+
+  def defend(self):
+    damage_blocked = 0
+    for armor in self.armor:
+      damage_blocked += armor.block()
+    return damage_blocked
+
   def fight(self, opponent):
-    #store fighters in a list then make a weighted choice based upon available stats
-    fighters = [opponent, self]
-    
-    #because random choices must return a list of string, we use an index to select the winner
-    index = [0,1]
-    winner = random.choices(index, weights=(opponent.current_health, self.current_health), k=1)
-    
-    #display results
-    print(f"In a duel between {fighters[0].name} and {fighters[1].name}...\nThe Victor!\nIs!\n{fighters[winner[0]].name}")
+    if(len(self.abilities) == 0 and len(opponent.abilities) == 0):
+      return print("draw")
+    opponent.alive = True
+    self.alive = True
+    while self.alive == True and opponent.alive == True:
+      opponent.current_health -= self.attack() - opponent.defend()
+      self.current_health -= opponent.attack() - self.defend()
+      self.alive = self.is_alive()
+      opponent.alive = opponent.is_alive()
+    if self.alive == False and opponent.alive == False:
+      return print(f"In a duel between {self.name} and {opponent.name}...\nThe result was a stalemate!")
+    elif self.alive == True:
+      return print(f"In a duel between {self.name} and {opponent.name}...\nThe Victor!\nIs!\n{self.name}")
+    elif opponent.alive == True:
+      return print(f"In a duel between {self.name} and {opponent.name}...\nThe Victor!\nIs!\n{opponent.name}")
+    else:
+      return print("error")
   def fight_random_opponent(self):
+
     #so that opponent is random, but never themselves
     #we loop until the opponent != self
     opponent = self
@@ -42,6 +66,18 @@ class Hero:
 
     #call fight method
     self.fight(opponent)
+  
+  def take_damage(self, damage_incoming):
+    damage_dealt = damage_incoming - self.defend()
+    if damage_dealt > 0:
+      self.current_health -= damage_dealt
+
+  def is_alive(self):
+    if self.current_health > 0:
+      return True
+    else:
+      return False
+
 
 #initialized  heroes    
 hero_1 = Hero("Flash")
